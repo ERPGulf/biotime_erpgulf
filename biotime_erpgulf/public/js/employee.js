9e0d8e6dc319928b91a68324aec603356d3c0e26
Harshafiroz <harsha@htsqatar.com>
2026-01-05 13:26:48 +0300

    prop removed

--- biotime_erpgulf/public/js/employee.js
+++ biotime_erpgulf/public/js/employee.js
@@ -1,17 +1,30 @@
+
 frappe.listview_settings['Employee'] = {
-    onload: function(listview) {
-        listview.page.add_inner_button(__('Sync Now'), function() {
+    onload: function (listview) {
+        listview.page.add_inner_button(__('Sync Now'), function () {
+
+            frappe.show_alert(
+                {
+                    message: __('BioTime employee sync has started'),
+                    indicator: 'blue'
+                },
+                5
+            );
+
             frappe.call({
-                method: "biotime_erpgulf.employee.sync_biotime_employees", 
-                callback: function(r) {
+                method: "biotime_erpgulf.employee.sync_biotime_employees",
+                callback: function (r) {
                     if (!r.exc) {
                         if (r.message && r.message.status === "success") {
-                            frappe.msgprint(__(r.message.message));  
+                            frappe.msgprint(__(r.message.message));
                         } else {
-                            frappe.msgprint(__('Employee Sync Completed'));
+                            frappe.msgprint(__('Employee sync request submitted successfully'));
                         }
                         listview.refresh();
                     }
+                },
+                error: function () {
+                    frappe.msgprint(__('Failed to start BioTime sync'));
                 }
             });
         });
